884274779512adea0b4e99edc1d679c85c0fc7f8
gstew <gstewman1@gmail.com>
2025-12-03 14:44:54 -0800

    Fixes
    Fixes to make the code work (mainly AndroidManifest)

--- app/src/main/java/com/example/project_02/LandingPage.java
+++ app/src/main/java/com/example/project_02/LandingPage.java
@@ -83,7 +83,6 @@ public class LandingPage extends AppCompatActivity {
     }
     private void setupButtons() {
         adminButton.setOnClickListener(v -> {
-
             Intent intent = new Intent(this, AdminActivity.class);
             startActivity(intent);
         });
